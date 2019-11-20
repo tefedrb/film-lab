9f033cf3e2302bdf86174f20c2fb4c3c0e31aa0d
tefedrb-enterprise <tefebell@gmail.com>
2019-11-19 23:41:19 -0500

    refactored fave into a functional component

--- src/Components/Fave.js
+++ src/Components/Fave.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import AddToQueueIcon from '@material-ui/icons/AddToQueue';
 
-class Fave extends Component {
-  handleClick = (e) => {
-    e.stopPropagation();
-    this.props.onFaveToggle()
-  }
+function Fave(props){
+    const handleClick = (e) => {
+      e.stopPropagation();
+      props.onFaveToggle()
+    }
 
-  render(){
-    const isFave = (this.props.isFave) ? 'remove_from_queue' : 'add_to_queue';
+    const isFave = (props.isFave) ? 'remove_from_queue' : 'add_to_queue';
     return (
-      <div onClick={this.handleClick} className={`film-row-fave ${isFave}`}>
+      <div onClick={handleClick} className={`film-row-fave ${isFave}`}>
         <AddToQueueIcon />
       </div>
     )
-  }
 }
 
 export default Fave;
